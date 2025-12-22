f449ca4a041cb061ac3e3eba69ca79d092d6e7ca
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2025-12-22 19:15:45 -0300

    Adicionando o primeiro exemplo

---
+++ capitulo_1/exemplo_1.py
@@ -0,0 +1,15 @@
+# Motivação impotética - Lista de amigos de pares de amizade na rede social datascientister
+
+# Nós temos uma rede social eu uma lista contendo dicionários com usuários, os seus nomes e ids. 
+# E temos uma lista contendo tuplas que indicam pares de amizade. 
+
+
+# Lista de usuários da datascientister. 
+
+lista_usuarios = [
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+
+]
+
