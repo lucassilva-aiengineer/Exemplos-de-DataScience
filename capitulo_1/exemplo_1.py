4c147771ef3660ad055dd106602446fa1c73d530
lucassilva-aiengineer <lucassilva.aiengineer@gmail.com>
2025-12-25 18:38:20 -0300

    Adicionando mais informações

--- capitulo_1/exemplo_1.py
+++ capitulo_1/exemplo_1.py
@@ -104,4 +104,96 @@ print(dicionario_amigos)
 
 # Definindo o número total de cone
 
-# Lembre -se 
+# Lembre - se Ciência de dados se trata estrair conhecimento de dados desorgânizado. 
+
+
+
+# Agora vamos criar funções que repondem algumas perguntas. 
+
+# Qual a quantidade de amigos que 1 id_ específico possui. 
+
+# 1)
+
+# Esta função conta o número de amigos que cada id possui 
+def quantidade_amigos(usuario_dict):
+
+    user_id = usuario_dict["id"]
+    id_amigos = dicionario_amigos[user_id]
+
+    return (user_id, len(id_amigos))
+
+
+# dicionarios_users = [{"id": 1}]
+# for dicionario in dicionarios_users:
+#     print(dicionario["id"])
+
+# Imprimindo a quantidade de amigos que o primeiro usuário possui. 
+def quantidade_amigos_a():
+    quantidade_amigos_indice_1 = quantidade_amigos(lista_usuarios[8])
+
+    print(quantidade_amigos_indice_1)
+
+# Agora eu vou somar a quantidade de conexões que  rede possui. 
+# total_conexoes = sum((quantidade_amigos(usuario) for usuario in lista_usuarios)) 
+
+# print("Possuimos um total de:",total_conexoes, "conexões.")
+
+
+# Descobrindo o número de conexões de uma forma mais simples. 
+
+def quantidade_conexoes():
+    quantidade_conexoes = 0 
+
+    for usuario in lista_usuarios:
+        quantidade__amigos_usuario = quantidade_amigos(usuario)
+
+        quantidade_conexoes += quantidade__amigos_usuario
+
+    print(f"Quantidade de conexões que a nossa rede social possui: {quantidade_conexoes} conexões.")
+
+    # 2) Quantidade média de conexões que cada usuário possui. 
+    # Vamos descobrir outra informação interessante.
+
+    # A quantidade total de conexões dividida pela quantidade de usuários. 
+
+def quantidade_media_conexoes(): 
+
+    pessoas = ["Mateus", "João"]
+    quantidade_pessoas = len(pessoas)
+    print("A quantidade de pessoas é: " + str(quantidade_pessoas))
+
+    numero_usuarios = len(lista_usuarios)
+    media_conexoes = quantidade_conexoes  / numero_usuarios 
+
+    print("A quantidade média de conexões que cada usuário possui em nossa rede social é: " + str(media_conexoes)+ "conexões / usuários")
+
+# Objetivo, extrair essas informações que extraimos da base de dados maior, treinar com um conjunto maior de dados, mais usuários isso é muito interessante. 
+
+
+# Próximo desafio identificar o usuário que possui o maior número de amigos, depois tentar identificar o usuário que possui menos amigos. 
+
+# Criando uma função que cria um ranking com as pessoas mais influentes da nossa rede social. 
+
+dicionario_ranking = {}
+
+# quantidade_usuarios_id = [] 
+for usuario in lista_usuarios:
+
+    tupla = quantidade_amigos(usuario)
+
+    # quantidade_usuarios_id.append(tupla) 
+
+    chave = tupla[0]
+    quantidade_usuarios = tupla[1]
+
+    dicionario_ranking[chave] = quantidade_usuarios 
+
+# print(quantidade_usuarios_id)
+
+# Vou pegar os índices das tuplas vou ordenar e exibir conforme a ordenação. 
+
+# quantidade_usuarios_id.sort()
+
+for id_ in range(0, 10):
+
+    print(f"Usuário {id_}: {dicionario_ranking[id_]}")
